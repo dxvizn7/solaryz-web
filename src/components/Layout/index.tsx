cab41873d5d01e23068b61c57f387416c4b8a255
dxvizn7 <davimendo09@gmail.com>
2026-03-02 20:27:52 -0300

    #PaginaLogin: só ajustes

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -7,7 +7,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <Sidebar />
       <div className="flex-1 flex flex-col h-screen overflow-hidden">
         <Header title='Dashboard' />
-        <main className="flex-1 p-8 overflow-y-auto">
+        <main className="flex-1 p-8 overflow-y-auto bg-solar-dark">
           {/* Tudo que estiver dentro do Layout vai renderizar aqui (Saldo, Extrato, etc) */}
           <div className="max-w-5xl mx-auto w-full flex flex-col gap-8">
             {children}
